3688abfdb3cda4ac59ff485f147f659ff787a9f8
Vladimir Korolev <vovsnmail@gmail.com>
2025-01-08 00:05:42 +0300

    load command

--- commands.py
+++ commands.py
@@ -2,7 +2,7 @@ import os
 from io import StringIO
 
 from telegram import Update
-from telegram.ext import CallbackContext, ContextTypes
+from telegram.ext import CallbackContext
 
 from logging_config import logger
 
@@ -13,53 +13,50 @@ MATERIALS_FOLDER = 'materials'
 async def results_command(update: Update, context: CallbackContext) -> None:
     user_username = update.message.from_user.username
     logger.info(f"User {user_username} triggered /results command.")
-    
-    # Correctly retrieve the user ID
     user_id = update.message.from_user.id
-    
-    # Check if the user is the teacher
     is_teacher = user_username == context.bot_data.get('teacher_username')
-    
-    # Teacher case: require argument
     if is_teacher:
         if not context.args:
-            logger.info(f"Missing test_id argument for /results command by {user_username}.")
+            logger.info(
+                f"Missing test_id arg for /results command by {user_username}."
+            )
             await update.message.reply_text("Пример: /results <test_id>")
             return
-        
         test_id = context.args[0]
         grades_file = os.path.join(GRADES_FOLDER, f'grades{test_id}.txt')
         if not os.path.exists(grades_file):
             logger.warning(f"Results file for test {test_id} not found.")
-            await update.message.reply_text(f"Нет результатов теста {test_id}.")
+            await update.message.reply_text(
+                f"Нет результатов теста {test_id}.")
             return
-        
-        logger.info(f"Found results for test {test_id}. Sending results to teacher {user_username}.")
+        logger.info(
+            f"Found results for test {test_id}.v"
+            f"Sending results to teacher {user_username}.")
         with open(grades_file, 'r', encoding='utf-8') as file:
             results = file.read()
         await update.message.reply_text(results)
-    
-    # Student case: no arguments allowed
     else:
         if context.args:
-            logger.info(f"Ignoring arguments for /results command by student {user_username}.")
-        
+            logger.info(
+                f"Ignoring args for /results command by {user_username}.")
         student_results_file = os.path.join('results', f'{user_id}.txt')
         if not os.path.exists(student_results_file):
-            logger.warning(f"Results file for student {user_username} not found.")
+            logger.warning(
+                f"Results file for student {user_username} not found.")
             await update.message.reply_text("У вас пока нет результатов.")
             return
-        
-        logger.info(f"Found results for student {user_username}. Sending results file.")
+        logger.info(
+            f"Found results for student {user_username}. Sending results file."
+        )
         try:
             with open(student_results_file, 'r', encoding='utf-8') as file:
                 results = file.read()
             await update.message.reply_text(f"Ваши результаты:\n\n{results}")
         except Exception as e:
-            logger.error(f"Error reading results for student {user_username}: {str(e)}")
-            await update.message.reply_text("Ошибка при получении ваших результатов.")
-
-
+            logger.error(
+                f"Error reading results for student {user_username}: {str(e)}")
+            await update.message.reply_text(
+                "Ошибка при получении ваших результатов.")
 
 
 async def txt_command(update: Update, context: CallbackContext) -> None:
@@ -70,30 +67,35 @@ async def txt_command(update: Update, context: CallbackContext) -> None:
         await update.message.reply_text("Только для преподавателя.")
         return
     if not context.args:
-        logger.info(f"Missing test_id argument for /txt command by {user_username}.")
+        logger.info(
+            f"Missing test_id argument for /txt command by {user_username}.")
         await update.message.reply_text("Пример: /txt 2")
         return
-    test_id = context.args[0].strip("'")  # Removing any surrounding single quotes
+    test_id = context.args[0].strip("'")
     grades_file = os.path.join(GRADES_FOLDER, f'grades{test_id}.txt')
     if not os.path.exists(grades_file):
         logger.warning(f"Results file for test {test_id} not found.")
         await update.message.reply_text(f"Нет результатов теста {test_id}.")
         return
-    logger.info(f"Found results for test {test_id}. Sending results as a document to user {user_username}.")
+    logger.info(
+        f"Found results for test {test_id}. "
+        f"Sending results as a document to user {user_username}.")
     with open(grades_file, 'r', encoding='utf-8') as file:
         results = file.read()
     titled_results = f"Результаты теста №'{test_id}'\n\n{results}"
     result_file = StringIO(titled_results)
-    result_file.name = f"results'{test_id}'.txt"  # Set the filename with single quotes
+    result_file.name = f"results'{test_id}'.txt"
     result_file.seek(0)
-    await update.message.reply_document(document=result_file, filename=f"results'{test_id}'.txt")
+    await update.message.reply_document(
+        document=result_file, filename=f"results'{test_id}'.txt")
 
 
 async def materials_command(update: Update, context: CallbackContext) -> None:
     user_username = update.message.from_user.username
     logger.info(f"User {user_username} triggered /materials command.")
     if not context.args:
-        logger.info(f"Missing test_id argument for /materials command by {user_username}.")
+        logger.info(
+            f"Missing test_id arg for /materials command by {user_username}.")
         await update.message.reply_text("Пример: /materials 2")
         return
     test_id = context.args[0]
@@ -108,26 +110,19 @@ async def materials_command(update: Update, context: CallbackContext) -> None:
         )
     ]
     if not files:
-        logger.warning(f"No files found in materials folder for test {test_id}.")
+        logger.warning(
+            f"No files found in materials folder for test {test_id}.")
         await update.message.reply_text(f"Нет материалов теста {test_id}.")
         return
-    logger.info(f"Found {len(files)} files for test {test_id}. Sending materials to user {user_username}.")
+    logger.info(
+        f"Found {len(files)} files for test {test_id}. "
+        f"Sending materials to user {user_username}."
+    )
     for file in files:
         file_path = os.path.join(materials_folder_id, file)
         try:
             await update.message.reply_document(document=open(file_path, 'rb'))
             logger.info(f"Sent file {file} to user {user_username}.")
         except Exception as e:
-            logger.error(f"Error sending file {file} to user {user_username}: {str(e)}")
-            await update.message.reply_text(f"Ошибка при отправке файла {file}.")
-
-
-async def start_command(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
-    help_text = """
-    Я - робот для тестирования студентов!
-    Примеры комманд:
-    /test 2 - запустить тест № 2
-    /materials 2 - получить учебные материалы по тесту № 2
-    /results - получить свои результаты
-    """
-    await update.message.reply_text(help_text)
+            logger.error(f"Error sending {file} to {user_username}: {str(e)}")
+            await update.message.reply_text(f"Ошибка при отправке {file}.")
