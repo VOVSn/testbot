060395a55c47b44e5a519c45b747f26c317cfa64
Vladimir Korolev <vovsnmail@gmail.com>
2025-01-08 20:20:37 +0300

    load command handler

--- main.py
+++ main.py
@@ -3,19 +3,18 @@ from telegram.ext import (
     CallbackQueryHandler, MessageHandler, filters
 )
 
+from settings import TOKEN, ADMIN_USERNAME, load_teachers
+from logging_config import logger
+
 from admin_commands import add_teacher_command, list_teachers_command
 from handlers import handle_message, error_handler
-from logging_config import logger
-from load_command import (
-    load_command, handle_file_upload, cancel_load, UPLOAD_STATE
-)
-from start_command import start_command_handler
+from start_handler import start_command_handler
 from test_handler import test_command, button_callback
-from settings import TOKEN, ADMIN_USERNAME, load_teachers
 from materials_handler import materials_command_handler
 from txt_handler import txt_command_handler
 from results_handler import results_command_handler
 from show_handler import show_command_handler
+from load_handler import load_command_handler
 
 
 def main():
@@ -25,27 +24,19 @@ def main():
         app.bot_data['admin_username'] = ADMIN_USERNAME
         app.bot_data['teacher_usernames'] = load_teachers()
 
-        load_handler = ConversationHandler(
-            entry_points=[CommandHandler('load', load_command)],
-            states={
-                UPLOAD_STATE: [MessageHandler(
-                    filters.Document.ALL, handle_file_upload)]
-            },
-            fallbacks=[CommandHandler('cancel', cancel_load)]
-        )
-
         handlers = [
             CommandHandler('test', test_command),
             CommandHandler('add', add_teacher_command),
             CommandHandler('list', list_teachers_command),
             CallbackQueryHandler(button_callback),
             MessageHandler(filters.TEXT & ~filters.COMMAND, handle_message),
-            load_handler,
+            # load_handler,
             materials_command_handler,
             txt_command_handler,
             results_command_handler,
             show_command_handler,
-            start_command_handler
+            start_command_handler,
+            load_command_handler
         ]
         
         for handler in handlers:
--- settings.py
+++ settings.py
@@ -1,28 +1,29 @@
 import os
+
 from dotenv import load_dotenv
 
 
 MATERIALS_FOLDER = 'materials'
 GRADES_FOLDER = 'grades'
-TESTS_FOLDER = 'tests'  # Folder containing test CSV files
+TESTS_FOLDER = 'tests'
+RESULTS_FOLDER = 'results'
 
-
-# Load environment variables from the .env file
 load_dotenv()
-
-# Environment variables
 TOKEN = os.getenv('TOKEN')
 BOT_USERNAME = os.getenv('BOT_USERNAME')
 ADMIN_USERNAME = os.getenv('ADMIN_USERNAME')
-TEACHER_USERNAMES_FILE = os.getenv('TEACHER_USERNAMES_FILE', 'teachers.txt')  # Default to 'teachers.txt' if not set
+TEACHER_USERNAMES_FILE = os.getenv('TEACHER_USERNAMES_FILE', 'teachers.txt')
 
 if not TOKEN or not BOT_USERNAME or not ADMIN_USERNAME:
-    raise ValueError("Missing required environment variables: TOKEN, BOT_USERNAME, or ADMIN_USERNAME")
+    raise ValueError(
+        "Missing required environment variables:"
+        " TOKEN, BOT_USERNAME, or ADMIN_USERNAME"
+    )
 
-# Function to load teacher usernames from the file
+
 def load_teachers():
     if not os.path.exists(TEACHER_USERNAMES_FILE):
         return []
-    
+
     with open(TEACHER_USERNAMES_FILE, 'r') as file:
         return [line.strip() for line in file.readlines()]
