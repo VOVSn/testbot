fb22a4d62901f654390b23f8c8c8504e1e01ad71
Vladimir Korolev <vovsnmail@gmail.com>
2025-01-08 22:57:18 +0300

    emoji friendly start command

--- handlers/test_handler.py
+++ handlers/test_handler.py
@@ -37,25 +37,36 @@ def load_questions(test_id: str) -> List[List[str]]:
 def get_next_question(context: CallbackContext) -> Tuple[str, InlineKeyboardMarkup]:
     current_question_index = context.user_data.get('current_question_index', 0)
     questions = context.user_data['questions']
-    if current_question_index < len(questions):
+    total_questions = len(questions)
+    
+    if current_question_index < total_questions:
         selected_line = questions[current_question_index]
-        logger.info(f"Fetching question {current_question_index + 1}.")
-        return updated_inline_keyboard(context, selected_line)
+        question_number = current_question_index + 1
+        return updated_inline_keyboard(context, selected_line, question_number, total_questions)
+    
     logger.info("No more questions to display.")
     return None, None
 
 
-def updated_inline_keyboard(context: CallbackContext, selected_line: List[str]) -> Tuple[str, InlineKeyboardMarkup]:
+def updated_inline_keyboard(context: CallbackContext, selected_line: List[str], question_number: int, total_questions: int) -> Tuple[str, InlineKeyboardMarkup]:
     question = selected_line[0]
     choices = selected_line[2:6]
     random.shuffle(choices)
     context.user_data['right_answer'] = selected_line[1]
+    
+    # Format the question display
+    formatted_question = (
+        f"*Вопрос* {question_number} *из* {total_questions}:\n"
+        "\n"
+        f"{question}\n"
+    )
+    
     keyboard = [
         [InlineKeyboardButton(choice, callback_data=choice) for choice in choices[:2]],
         [InlineKeyboardButton(choice, callback_data=choice) for choice in choices[2:]],
         [InlineKeyboardButton('Отмена', callback_data='cancel')],
     ]
-    return question, InlineKeyboardMarkup(keyboard)
+    return formatted_question, InlineKeyboardMarkup(keyboard)
 
 
 async def handle_cancel(query, context):
@@ -75,9 +86,8 @@ async def handle_cancel(query, context):
 
 async def handle_answer(query, context):
     right_answer = context.user_data.get('right_answer')
-    user_data = context.user_data.setdefault(
-        'test_results', {'correct': 0, 'total': 0}
-    )
+    user_data = context.user_data.setdefault('test_results', {'correct': 0, 'total': 0})
+    
     if query.data == right_answer:
         logger.info(f"User {query.from_user.username} answered correctly.")
         await query.answer('ВЕРНО!')
@@ -85,13 +95,37 @@ async def handle_answer(query, context):
     else:
         logger.info(f"User {query.from_user.username} answered incorrectly.")
         await query.answer('НЕВЕРНО')
+    
     user_data['total'] += 1
     context.user_data['current_question_index'] += 1
+    
     question, reply_markup = get_next_question(context)
     if question:
-        await query.edit_message_text(question, reply_markup=reply_markup)
+        await query.edit_message_text(
+            question, reply_markup=reply_markup, parse_mode='Markdown')
     else:
         await display_results(query, context)
+
+
+async def test_command(update: Update, context: CallbackContext):
+    if not context.args:
+        await update.message.reply_text(
+            'Пожалуйста, введите номер теста, например: /test 2')
+        return
+    
+    test_id = context.args[0]
+    try:
+        initialize_test_session(context, test_id)
+        question, reply_markup = get_next_question(context)
+        if question:
+            message = await update.message.reply_text(
+                question, reply_markup=reply_markup, parse_mode='Markdown')
+            context.user_data['last_question_message_id'] = message.message_id
+        else:
+            await update.message.reply_text('Нет вопросов в тесте!')
+    except FileNotFoundError:
+        logger.error(f"Test {test_id} not found.")
+        await update.message.reply_text(f'Тест {test_id} не найден.')
 
 
 async def display_results(query, context):
@@ -121,26 +155,6 @@ async def display_results(query, context):
     await query.edit_message_text(
         f'Вы завершили тест, правильно {correct} из {total} [{percentage_str}%].')
     context.user_data.clear()
-
-
-async def test_command(update: Update, context: CallbackContext):
-    if not context.args:
-        await update.message.reply_text(
-            'Пожалуйста, введите номер теста, например: /test 2')
-        return
-    test_id = context.args[0]
-    try:
-        initialize_test_session(context, test_id)
-        question, reply_markup = get_next_question(context)
-        if question:
-            message = await update.message.reply_text(
-                question, reply_markup=reply_markup)
-            context.user_data['last_question_message_id'] = message.message_id
-        else:
-            await update.message.reply_text('Нет вопросов в тесте!')
-    except FileNotFoundError:
-        logger.error(f"Test {test_id} not found.")
-        await update.message.reply_text(f'Тест {test_id} не найден.')
 
 
 async def button_callback(update: Update, context: CallbackContext):
